6f45916dd8ffb24e2f1256fac3fe706b50484563
hjiang2 <32148302+hjiang2@users.noreply.github.com>
2018-02-08 11:20:28 -0600

    a simple monster class

---
+++ src/Adventure2/Monster.java
@@ -0,0 +1,35 @@
+package Adventure2;
+
+public class Monster {
+    private String name;
+    private Double attack;
+    private Double defense;
+    private Double health;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Double getAttack() {
+        return attack;
+    }
+    public void setAttack(Double attack) {
+        this.attack = attack;
+    }
+    public Double getDefense() {
+
+        return defense;
+    }
+    public void setDefense(Double defense) {
+        this.defense = defense;
+    }
+    public Double getHealth() {
+        return health;
+    }
+    public void setHealth(Double health) {
+        this.health = health;
+    }
+    
+}
